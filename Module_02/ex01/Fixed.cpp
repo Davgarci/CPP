927f76e39da022df4f907aad6e547daf4c4ba7fb
David Garcia lou <davgarci@c2r9s2.42madrid.com>
2023-04-06 22:56:53 +0200

    ex01 finish

--- Module_02/ex01/Fixed.cpp
+++ Module_02/ex01/Fixed.cpp
@@ -6,35 +6,59 @@
 /*   By: davgarci <davgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 13:25:03 by davgarci          #+#    #+#             */
-/*   Updated: 2023/04/05 21:10:42 by davgarci         ###   ########.fr       */
+/*   Updated: 2023/04/06 22:54:57 by davgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Fixed.hpp"
-#include <iostream>
 
 Fixed::Fixed(void){
 	std::cout << "Default constructor called" << std::endl;
 	_full = 0;
 }
 
+Fixed::Fixed(const int i){
+	_full = (i << _fractional_bit);
+	std::cout << "Int constructor called" << std::endl;
+}
+
+// 1 << _fractional_bit es 256 que es como mover 8 bits a la izq
+Fixed::Fixed(const float i){
+	_full = (i * (1 << _fractional_bit));
+	std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed::Fixed(Fixed &hello){
+Fixed::Fixed(Fixed const &hello){
 	std::cout << "Copy constructor called" << std::endl;
-	 this->_full = hello._full;
+	 *this = hello;
 }
 
-Fixed& Fixed::operator=(Fixed &hello){
+Fixed& Fixed::operator=(Fixed const &hello){
 	std::cout << "Assignation operator called" << std::endl;
 	this->_full = hello.getRawBits();
 	return(*this);
 }
 
+std::ostream& operator<< (std::ostream& os, const Fixed& str){
+	os << str.toFloat();
+	return(os);
+}
+
+float Fixed::toFloat( void ) const{
+	float a = (float)_full / 256;
+	return(a);
+}
+
+int Fixed::toInt( void ) const{
+	int a = _full / 256;
+	return(a);
+}
+
 int Fixed::getRawBits( void ) const{
-	std::cout << "getRawBits member function called" << std::endl;
 	return(this->_full);
 }
 
--- Module_02/ex01/Fixed.hpp
+++ Module_02/ex01/Fixed.hpp
@@ -6,11 +6,12 @@
 /*   By: davgarci <davgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 13:25:06 by davgarci          #+#    #+#             */
-/*   Updated: 2023/04/05 21:32:44 by davgarci         ###   ########.fr       */
+/*   Updated: 2023/04/06 20:19:22 by davgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # pragma once
+#include <iostream>
 
 class Fixed {
 	private:
@@ -19,10 +20,15 @@ class Fixed {
 	public:
 		Fixed(void);
 		~Fixed();
-		Fixed(Fixed &hello);
-		Fixed(int i);
-		Fixed& operator=(Fixed &hello);
+		Fixed(Fixed const &hello);
+		Fixed(const int i);
+		Fixed(const float i);
+		Fixed& operator=(Fixed const &hello);
 
+		float toFloat( void ) const;
+		int toInt( void ) const;
 		int getRawBits( void ) const;
 		void setRawBits ( int const raw );
 };
+
+		std::ostream& operator<< (std::ostream& os, const Fixed& str);
